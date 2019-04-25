f43f648abcb4b4d35671ab73e0c34dc1eec7382a
petr-konovalov <petrkon98@mail.ru>
2019-04-25 21:35:08 +0300

    directories redistribution

--- MATLAB/Робо-Футбол/defenceGoalFor2by2.m
+++
@@ -1,8 +0,0 @@
-function [rul] = defenceGoalFor2by2(agent, oppAttacker, G)
-    minSpeed = 15;
-    P = 4/750;
-    D = -1.5;
-    vicinity = 50;
-    
-    rul = MoveToPD(agent, (oppAttacker.z + G) / 2, minSpeed, P, D, vicinity);
-end
